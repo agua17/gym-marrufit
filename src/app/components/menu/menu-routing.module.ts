56b7eb0023933d4ab21add13d02f15b38ab3ba35
Agua <aguassantasnunez@gmail.com>
2024-06-04 15:23:21 +0200

    changed_component

--- src/app/components/menu/menu-routing.module.ts
+++ src/app/components/menu/menu-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('../../pages/home-page/home-page.module').then(
+        (m) => m.HomePageModule
+      ),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('../../pages/login-page/login-page.module').then(
+        (m) => m.LoginPageModule
+      ),
+  },
+  {
+    path: 'horarios',
+    loadChildren: () =>
+      import('../../pages/timetable-page/timetable-page.module').then(
+        (m) => m.TimetablePageModule
+      ),
+  },
+  {
+    path: 'conocenos',
+    loadChildren: () =>
+      import('../../pages/history-page/history-page.module').then(
+        (m) => m.HistoryPageModule
+      ),
+  },
+  {
+    path: 'servicios',
+    loadChildren: () =>
+      import('../../pages/activities-page/activities-page.module').then(
+        (m) => m.ActivitiesPageModule
+      ),
+  },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
