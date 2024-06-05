0de6a923fe4f540d84977436e6635b0994283bce
Agua <aguassantasnunez@gmail.com>
2024-06-05 22:39:06 +0200

    changed_component_home

--- src/app/components/intro/intro.module.ts
+++ src/app/components/intro/intro.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { IntroRoutingModule } from './intro-routing.module';
 import { IntroComponent } from './intro/intro.component';
+import { MaterialModule } from '../../material/material.module';
 
 @NgModule({
   declarations: [IntroComponent],
-  imports: [CommonModule, IntroRoutingModule],
+  imports: [CommonModule, IntroRoutingModule, MaterialModule],
   exports: [IntroComponent],
 })
 export class IntroModule {}
