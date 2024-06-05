f48f9427ec2eed31eeae2664c615f50400fd0973
Agua <aguassantasnunez@gmail.com>
2024-06-05 18:45:00 +0200

    solution_component_services

--- src/app/components/gymsesion/gymsesion/gymsesion.component.ts
+++ src/app/components/gymsesion/gymsesion/gymsesion.component.ts
@@ -1,6 +1,5 @@
 import { sesion } from './../interfaces/gymsesion.interface';
 import { Component, Input, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-gymsesion',
   templateUrl: './gymsesion.component.html',
@@ -8,7 +7,27 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GymsesionComponent implements OnInit {
   @Input()
-  public item!: sesion;
+  public sesion = [
+    {
+      title: 'ENTRENO PERSONAL',
+      icon: "''",
+      description:
+        '¿No sabes entrenar solo? Este es tu servicio, ya que estarás acompañado en todo tu entramiento por un profesional que te ayudará y guiará en toda la sesión',
+      price: '68€/mes',
+    },
+    {
+      icon: 'path/to/icon1.png',
+      title: 'Servicio 1',
+      description: 'Descripción breve del servicio 1.',
+      price: '$20/mes',
+    },
+    {
+      icon: 'path/to/icon1.png',
+      title: 'Servicio 1',
+      description: 'Descripción breve del servicio 1.',
+      price: '$20/mes',
+    },
+  ];
 
   ngOnInit(): void {}
 }
--- src/app/pages/activities-page/activities-page/activities-page.component.ts
+++ src/app/pages/activities-page/activities-page/activities-page.component.ts
@@ -1,30 +1,8 @@
 import { Component } from '@angular/core';
-
+import { sesion } from '../../../components/gymsesion/interfaces/gymsesion.interface';
 @Component({
   selector: 'app-activities-page',
   templateUrl: './activities-page.component.html',
   styleUrls: ['./activities-page.component.scss'],
 })
-export class ActivitiesPageComponent {
-  public sesion = [
-    {
-      title: 'ENTRENO PERSONAL',
-      description:
-        '¿No sabes entrenar solo? Este es tu servicio, ya que estarás acompañado en todo tu entramiento por un profesional que te ayudará y guiará en toda la sesión',
-      price: '68€/mes',
-    },
-    {
-      icon: 'path/to/icon1.png',
-      title: 'Servicio 1',
-      description: 'Descripción breve del servicio 1.',
-      price: '$20/mes',
-    },
-    {
-      icon: 'path/to/icon1.png',
-      title: 'Servicio 1',
-      description: 'Descripción breve del servicio 1.',
-      price: '$20/mes',
-    },
-  ];
-  item: any;
-}
+export class ActivitiesPageComponent {}
